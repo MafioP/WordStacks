44568d8b94fc193d41b3c611c6d3766d048dff53
zerebritvs <58092389+zerebritvs@users.noreply.github.com>
2019-11-24 21:14:59 +0100

    he cambiado listapalabras y me queda hacer el random sin repetirse

--- WordStacks/src/Diccionario.java
+++ WordStacks/src/Diccionario.java
@@ -1,15 +1,30 @@
 
 public class Diccionario {
 	private static String[] diccionario() {
-		String [] listapalabras = {"ORDENADOR", "MONITOR", "TECLADO", "RATON", "ALTAVOZ", "DISCO", "GRAFICA", "PROGRAMA", "CODIGO", "ALGORITMO", "METODO", "FUNCION", "INTERFAZ", "VARIABLE", "CLASE", "MATRIZ", ""};
+		String [] listapalabras = {"ORDENADOR", "MONITOR", "TECLADO", "RATON", "ALTAVOZ", "DISCO", "GRAFICA", "PROGRAMA", "CODIGO", "ALGORITMO", "METODO", "FUNCION", "INTERFAZ", "VARIABLE", "CLASE", "MATRIZ", "ARCHIVO","FICHERO","CARPETA","CODIGO","EJECUTAR","ENCRIPTAR","ERROR","HACKER","RED","SISTEMA","USUARIO","WIFI","VIRUS","WEB","DIGITAL"};
 		return listapalabras;
 	}
-	public static String[] listaPrubea() {
-		String [] listaprueba = {"ORDENADOR","MONITOR", "TECLADO", "RATON"};
+	public static String[] listaPrueba() {
+		String [] listaprueba = {"AAA", "AAAAAA", "BBBBBBBBB", "CCCCC",
+				"DDDDD", "EEEEE", "FFFFF", "GGGGG",
+				"HHHHH", "IIIII", "JJJJJ"};
 		return listaprueba;
 	}
-	public static String[] listaNormal() { // 10 palabras aleatorias de diccionario
-		String [] listanormal = {""};
-		return listanormal;
+	public static String[] listaNormal(String [] listapalabras) { // 10 palabras aleatorias de diccionario
+	
+		String [] listanormal = diccionario();
+		String[] palabras = {""};
+		
+		for(int i=0;i<10;i++) {
+			int rand;
+			
+			rand = (int)Math.floor(Math.random()*listanormal.length);
+			palabras[i] = listanormal[rand];
+			for(j=1;j<1;j++){
+				
+			}
+			
+		}
+		return palabras;
 	}
 }
