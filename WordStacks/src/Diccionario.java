da0902ab1781ff6ee0976895badcaa0b0a4261b6
MafioP <42208309+MafioP@users.noreply.github.com>
2019-11-26 19:50:50 +0100

    Update Diccionario.java

--- WordStacks/src/Diccionario.java
+++ WordStacks/src/Diccionario.java
@@ -1,7 +1,8 @@
 
 public class Diccionario {
 	private static String[] diccionario() {
-		String [] listapalabras = {"ORDENADOR", "MONITOR", "TECLADO", "RATON", "ALTAVOZ", "DISCO", "GRAFICA", "PROGRAMA", "CODIGO", "ALGORITMO", "METODO", "FUNCION", "INTERFAZ", "VARIABLE", "CLASE", "MATRIZ", ""};
+		String [] listapalabras = {"ORDENADOR", "MONITOR", "TECLADO", "RATON", "ALTAVOZ", "DISCO", "GRAFICA", "PROGRAMA", "CODIGO", "ALGORITMO", "METODO", "FUNCION", "INTERFAZ", "VARIABLE", "CLASE", "MATRIZ", 
+				"ARCHIVO","FICHERO","CARPETA","CODIGO","EJECUTAR","ENCRIPTAR","ERROR","HACKER","RED","SISTEMA","USUARIO","WIFI","VIRUS","WEB","DIGITAL"};
 		return listapalabras;
 	}
 	public static String[] listaPrubea() {
