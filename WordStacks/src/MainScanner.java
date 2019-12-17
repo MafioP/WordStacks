de241957ffbce048bf1e7576c0ffacbcb15b830e
MafioP <42208309+MafioP@users.noreply.github.com>
2019-12-17 21:53:04 +0100

    pattern matcher

--- WordStacks/src/MainScanner.java
+++ WordStacks/src/MainScanner.java
@@ -1,32 +1,52 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainScanner {
+	
+	/**
+	 * lee la entrada y comprueba si es valida para ser usada en el resto del programa
+	 * @return
+	 */
 	public static String readInput() {
 		String invalidInput = "0";
 		String readCoords;
 		Scanner in = new Scanner(System.in);
 		readCoords = in.next();
 		
-		if(readCoords.length() <=4 && readCoords.length() != 2) {
-			switch(readCoords.length()) {
-			case 4:
-				try {
-					int x = Character.getNumericValue(readCoords.charAt(0));
-					int y = Character.getNumericValue(readCoords.charAt(1));
-					char o = readCoords.charAt(2);
-					int l = Character.getNumericValue(readCoords.charAt(3));
-					System.out.println("valid");
-					return readCoords;
-				}catch (Exception e) {
-					e.printStackTrace();
-					return invalidInput;
+		if (readCoords.length() <=4 && readCoords.length() != 2) {
+			switch (readCoords.length()) {
+				case 4:
+					if (validate(readCoords)) {
+						return readCoords;
+					} else {
+						System.out.println("Entrada no valida");
+					}
+					break;
+				default:
+					System.out.println("Caso pendiente");
 				}
-			}
 			
 		} else {
 			System.out.println("Entrada no valida");
 		}
+		
 
 		return invalidInput;
+	}
+	
+	/**
+	 * compara el input con el pattern (int,int,char,int) y devuelve true si es valido
+	 * @param input
+	 * @return
+	 */
+	private static boolean validate(String input) {
+		if (input == null) {
+			return false;
+		}
+		Pattern pattern = Pattern.compile("\\d\\d([NSEO]|[nseo])\\d");
+		Matcher matcher = pattern.matcher(input);
+		
+		return matcher.matches();
 	}
 }
